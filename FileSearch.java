df2db23768860d7010e7aac235655fcf67e62d88
KamronSoldozy <2018ksoldozy@tjhsst.edu>
2016-02-11 23:34:27 -0500

    Edits
    New Edits

--- FileSearch.java
+++ FileSearch.java
@@ -4,45 +4,13 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.io.*;
 import java.util.*;
-
+//Created by Kamron Soldozy 
 public class FileSearch {
    public static File a;
    public static File[] b;
    public static String search;
     
    public static void main(String[] args) {
-      
-      //try{ 
-         /*URI uri = new URI("http://ksoldozy.me");
-      
-         Desktop desktop = Desktop.getDesktop();
-         desktop.browse(uri);
-         File x = new File("C:\\Users\\Kamron\\Desktop\\my fav table.pdf");
-         File z = new File("C:\\Users\\Kamron\\Documents\\Credits.txt");
-         desktop.open(x);
-         desktop.open(z);
-         */
-      //}
-        /*
-      File x =new File("C:\\Users");
-      File[] z = x.listFiles();
-      for(int j = 0; j < z.length; j++)
-         System.out.println(z[j]);
-      File[] a = z[5].listFiles();
-      for(int b = 0; b < a.length; b++)
-         System.out.println(a[b]);
-      */    
-      /*
-      catch (IOException ioe) {
-         ioe.printStackTrace();
-      } 
-      catch (URISyntaxException use) {
-         use.printStackTrace();
-      }
-      */
-      
-      
-      
       
       System.out.println("What file are you looking for? (Include Extension name)");
       Scanner infile = new Scanner(System.in);
